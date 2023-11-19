ffb8bddedc99b59790036a2adfd00b4d4c77e968
Joel Fankam <37833979+joekrom@users.noreply.github.com>
2023-11-19 22:53:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 # Set up Cloud Foundry API, organization, and space
-if cf login -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"; then
+if cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"; then
     # If login is successful, set the target organization and space
     cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
     echo "Connection to Cloud Foundry successful" >> "$GITHUB_OUTPUT"
